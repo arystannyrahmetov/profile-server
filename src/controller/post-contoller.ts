b9c60568e8d4358f428bf9a4c80bd8c2d07ac3db
arystannyrahmetov <arystannyrahmetov@gmail.com>
2022-08-28 23:12:02 +0600

    initial

--- src/controller/post-contoller.ts
+++ src/controller/post-contoller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Param } from 'routing-controllers';
 import {MongoClient} from "mongodb";
-import confidential from "./db-data";
 import 'reflect-metadata';
+import dotenv from "dotenv";
+dotenv.config();
 
-const uri = `mongodb+srv://${confidential.username}:${confidential.password}@maincluster.z1mycci.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.PASSWORD}@maincluster.z1mycci.mongodb.net/?retryWrites=true&w=majority`;
 // @ts-ignore
 const client = new MongoClient(uri, { "useNewUrlParser": true, "useUnifiedTopology": true });
 
@@ -16,7 +17,7 @@ export class PostController {
     @Get('/posts')
     async getAll () {
         //let posts = await client.db(confidential.database).collection(confidential.collection).find().toArray();
-        let posts = await client.db(confidential.database).collection(confidential.collection).aggregate([{
+        let posts = await client.db(process.env.DATABASE).collection(process.env.POSTSCOLLECTION ?? "posts").aggregate([{
             $project: {
                 _id: {
                     $toString: "$_id"
